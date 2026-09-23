e98875db40b3c6733ea06a7f7ac231eac50cf8ed
Cyn-sharp <cyndrickabejo@gmail.com>
2026-09-23 23:38:46 +0800

    atm

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,14 +4,46 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Enter your grade: ");
-        double grade = Convert.ToDouble(Console.ReadLine());
+        double balance = 5000;
 
-        if (grade >= 90)
-            Console.WriteLine("Excellent!");
-        else if (grade >= 75)
-            Console.WriteLine("Passed!");
+        Console.WriteLine("=== SIMPLE ATM ===");
+        Console.WriteLine("1. Check Balance");
+        Console.WriteLine("2. Deposit");
+        Console.WriteLine("3. Withdraw");
+
+        Console.Write("Choose: ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        if (choice == 1)
+        {
+            Console.WriteLine($"Balance: ₱{balance:N2}");
+        }
+        else if (choice == 2)
+        {
+            Console.Write("Deposit amount: ");
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            balance += amount;
+            Console.WriteLine($"New balance: ₱{balance:N2}");
+        }
+        else if (choice == 3)
+        {
+            Console.Write("Withdraw amount: ");
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            if (amount <= balance)
+            {
+                balance -= amount;
+                Console.WriteLine($"New balance: ₱{balance:N2}");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance.");
+            }
+        }
         else
-            Console.WriteLine("Failed.");
+        {
+            Console.WriteLine("Invalid choice.");
+        }
     }
 }

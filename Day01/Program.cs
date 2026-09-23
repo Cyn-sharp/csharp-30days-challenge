ccc774cb9751d306322fcbf747ddd06f7acdd3ea
Cyn-sharp <cyndrickabejo@gmail.com>
2026-09-23 23:40:27 +0800

    shop cart total

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,24 +4,20 @@ class Program
 {
     static void Main()
     {
-        Random random = new Random();
-        int secretNumber = random.Next(1, 101);
-        int guess = 0;
+        string[] products = { "Keyboard", "Mouse", "Headset" };
+        double[] prices = { 850, 450, 1200 };
 
-        Console.WriteLine("=== NUMBER GUESSING GAME ===");
-        Console.WriteLine("Guess a number from 1 to 100.");
+        double total = 0;
 
-        while (guess != secretNumber)
+        Console.WriteLine("=== SHOPPING CART ===");
+
+        for (int i = 0; i < products.Length; i++)
         {
-            Console.Write("Enter your guess: ");
-            guess = Convert.ToInt32(Console.ReadLine());
-
-            if (guess < secretNumber)
-                Console.WriteLine("Too low!");
-            else if (guess > secretNumber)
-                Console.WriteLine("Too high!");
-            else
-                Console.WriteLine("Correct! 🎉");
+            Console.WriteLine($"{products[i]} - ₱{prices[i]:N2}");
+            total += prices[i];
         }
+
+        Console.WriteLine("---------------------");
+        Console.WriteLine($"Total: ₱{total:N2}");
     }
 }

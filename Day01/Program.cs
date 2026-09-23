a8c93fb400915fb5d573ae6772c8dacca48f59c6
Cyn-sharp <cyndrickabejo@gmail.com>
2026-09-23 23:49:25 +0800

    yes

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,36 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Create a password: ");
-        string password = Console.ReadLine();
-
-        bool hasMinimumLength = password.Length >= 8;
-        bool hasNumber = false;
-
-        foreach (char character in password)
-        {
-            if (char.IsDigit(character))
-            {
-                hasNumber = true;
-                break;
-            }
-        }
-
-        Console.WriteLine("\n=== PASSWORD CHECK ===");
-
-        if (hasMinimumLength && hasNumber)
-        {
-            Console.WriteLine("Strong password!");
-        }
-        else
-        {
-            Console.WriteLine("Weak password.");
-
-            if (!hasMinimumLength)
-                Console.WriteLine("- Must contain at least 8 characters.");
-
-            if (!hasNumber)
-                Console.WriteLine("- Must contain at least one number.");
-        }
+        Console.Write("Hello World");
     }
+
 }

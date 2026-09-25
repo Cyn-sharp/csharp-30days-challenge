edec95ea605fe35b8ef41a63198bfa9d26597b09
Cyn-sharp <cyndrickabejo@gmail.com>
2026-09-25 18:52:39 +0800

    Practice about methods

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,7 +4,15 @@ namespace Day1
 {
     class Program
     {
-        
-        
+        static void MyMethod(string fname, int age)
+        {
+            Console.WriteLine(fname + " is " + age);
+        }
+        static void Main()
+        {
+            MyMethod("Cyndrick", 16);
+            MyMethod("Rusty", 18);
+            MyMethod("Hezelle", 18);
+        }
     }
 }
